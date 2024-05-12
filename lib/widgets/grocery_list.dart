c0a4b4be461e69187a4263f96ca063a78f6df006
ANKIT KUMAR <ankitwork139@gmail.com>
2024-05-12 16:55:59 +0530

    create a form to add new items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({Key? key}) : super(key: key);
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void addItem() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your Groceries')),
+      appBar: AppBar(
+        title: Text('Your Groceries'),
+        actions: [IconButton(onPressed: addItem, icon: const Icon(Icons.add))],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder: (context, index) => ListTile(
---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({Key? key}) : super(key: key);
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  final formKey = GlobalKey<FormState>();
+  var enteredName = '';
+  var enteredQuantity = 1;
+  var selectedCategory = categories[Categories.vegetables]!;
+  void saveItem() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a new item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  maxLength: 50,
+                  decoration: InputDecoration(label: Text('Name')),
+                  validator: (Value) {
+                    if (Value == null ||
+                        Value.isEmpty ||
+                        Value.trim().length <= 1 ||
+                        Value.trim().length >= 50) {
+                      return 'Must be between 1 to 50 characters.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    // if (value == null) {
+                    //   return;
+                    // }
+                    enteredName = value!;
+                  },
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          label: Text('Quantity'),
+                        ),
+                        keyboardType: TextInputType.number,
+                        initialValue: enteredQuantity.toString(),
+                        validator: (Value) {
+                          if (Value == null ||
+                              Value.isEmpty ||
+                              int.tryParse(Value) == null ||
+                              int.tryParse(Value)! <= 0) {
+                            return 'Must be a valid positive number.';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          enteredQuantity = int.parse(value!);
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      child: DropdownButtonFormField(
+                        value: selectedCategory,
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: category.value,
+                              child: Row(children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.title),
+                              ]),
+                            ),
+                        ],
+                        onChanged: (Category? value) {
+                          setState(() {
+                            selectedCategory = value ?? selectedCategory;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null) {
+                            return 'Please select a category.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        formKey.currentState!.reset();
+                      },
+                      child: const Text('Reset'),
+                    ),
+                    SizedBox(width: 15),
+                    ElevatedButton(
+                      onPressed: saveItem,
+                      child: const Text('Add Item'),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
